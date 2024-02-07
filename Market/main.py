c2efc4841385f37c60c995381bb8b3552f4fce40
Hunafarizal <hunafarizal@gmail.com>
2024-02-07 17:44:02 +0700

    Excercise Upload

---
+++ Market/main.py
@@ -0,0 +1,11 @@
+Apple = int(input('input apple values :'))
+Orange = int(input('input orange values :'))
+Grape = int(input('input grape values :'))
+
+Apple_Price = 10000
+Orange_Price = 15000
+Grape_Price = 20000
+
+total = (Apple_Price * Apple) + (Orange_Price * Orange) + (Grape_Price * Grape)
+
+print(total)
